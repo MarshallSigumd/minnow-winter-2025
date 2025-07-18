4d196aa4c153949a98400593c0391176f5e20885
dlzwtk <2020694283@qq.com>
2025-07-18 18:13:30 +0800

    lab0 finished

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -6,51 +6,63 @@ ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
 
 void Writer::push( string data )
 {
-  (void)data; // Your code here.
+  // Push data to the buffer, but only as much as available capacity allows.
+  uint64_t to_write = min( available_capacity(), data.size() );
+  buffer_.append( data, 0, to_write );
+  bytes_pushed_ += to_write;
 }
 
 void Writer::close()
 {
-  // Your code here.
+  is_closed_ = true;
 }
 
 bool Writer::is_closed() const
 {
-  return {}; // Your code here.
+  return is_closed_;
 }
 
 uint64_t Writer::available_capacity() const
 {
-  return {}; // Your code here.
+  return capacity_ - ( buffer_.size() - read_index_ );
 }
 
 uint64_t Writer::bytes_pushed() const
 {
-  return {}; // Your code here.
+  return bytes_pushed_;
 }
 
 string_view Reader::peek() const
 {
-  return {}; // Your code here.
+  // Return a string_view of the data in the buffer.
+  return string_view( buffer_.data() + read_index_, buffer_.size() - read_index_ );
 }
 
 void Reader::pop( uint64_t len )
 {
-  (void)len; // Your code here.
+  // Pop as much data as possible from the buffer if len is greater than the amount of data available.
+  uint64_t to_read = min( bytes_buffered(), len );
+  read_index_ += to_read;
+  bytes_popped_ += to_read;
+
+  // When more than half of the buffer is read, shrink it to avoid unnecessary memory usage.
+  if ( read_index_ >= buffer_.size() / 2 ) {
+    buffer_ = buffer_.substr( read_index_ );
+    read_index_ = 0;
+  }
 }
 
 bool Reader::is_finished() const
 {
-  return {}; // Your code here.
+  return is_closed_ && bytes_buffered() == 0;
 }
 
 uint64_t Reader::bytes_buffered() const
 {
-  return {}; // Your code here.
+  return buffer_.size() - read_index_;
 }
 
 uint64_t Reader::bytes_popped() const
 {
-  return {}; // Your code here.
+  return bytes_popped_;
 }
-
