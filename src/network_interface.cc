2fda6fd65d4e3d7f9524a9597c01e597e964797a
dlzwtk <2020694283@qq.com>
2025-08-28 16:09:19 +0800

    lab5 finished

--- src/network_interface.cc
+++ src/network_interface.cc
@@ -30,20 +30,110 @@ NetworkInterface::NetworkInterface( string_view name,
 //! can be converted to a uint32_t (raw 32-bit IP address) by using the Address::ipv4_numeric() method.
 void NetworkInterface::send_datagram( const InternetDatagram& dgram, const Address& next_hop )
 {
-  debug( "unimplemented send_datagram called" );
-  (void)dgram;
-  (void)next_hop;
+  // debug( "unimplemented send_datagram called" );
+  // (void)dgram;
+  // (void)next_hop;
+
+  EthernetFrame eframe;
+
+  // If the destination Ethernet address is already known, create a Ethernet frame and send it right away
+  if ( getMapping( next_hop.ipv4_numeric(), eframe.header.dst ) ) {
+    // Set up the Ethernet frame header
+    eframe.header.type = EthernetHeader::TYPE_IPv4;
+    eframe.header.src = ethernet_address_;
+
+    // Set up the Ethernet frame payload to be the serialized datagram
+    Serializer serializer;
+    dgram.serialize( serializer );
+    eframe.payload = serializer.finish();
+
+    // Send the Ethernet frame
+    transmit( eframe );
+    return;
+  }
+
+  // The destination Ethernet address is unknown,
+  // broadcast an ARP request and queue the datagram
+  EthernetFrame arp_request = create_arp_message( ARPMessage::OPCODE_REQUEST,
+                                                  ethernet_address_,
+                                                  ip_address_.ipv4_numeric(),
+                                                  { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 },
+                                                  next_hop.ipv4_numeric() );
+  if ( can_send_arp_request( next_hop.ipv4_numeric() ) ) {
+    transmit( arp_request );
+    update_arp_request_time( next_hop.ipv4_numeric() );
+  }
+  QueuedDatagram qdgram;
+  qdgram.next_hop_ip = next_hop.ipv4_numeric();
+  qdgram.dgram = dgram;
+  datagrams_queued_.insert( make_pair( time_elapsed_, qdgram ) );
 }
 
 //! \param[in] frame the incoming Ethernet frame
 void NetworkInterface::recv_frame( EthernetFrame frame )
 {
-  debug( "unimplemented recv_frame called" );
-  (void)frame;
+  // debug( "unimplemented recv_frame called" );
+  // (void)frame;
+
+  // Ignore any frames not destined for the network interface
+  if ( frame.header.dst != ethernet_address_ && frame.header.dst != ETHERNET_BROADCAST ) {
+    return;
+  }
+
+  // If inbound frame is IPv4, parse the payload as an InternetDatagram
+  // and if successful, push the resulting datagram to the datagrams_received_ queue
+  if ( frame.header.type == EthernetHeader::TYPE_IPv4 ) {
+    Parser parser( frame.payload );
+
+    InternetDatagram dgram;
+    dgram.parse( parser );
+    if ( parser.has_error() )
+      return;
+    datagrams_received_.push( dgram );
+  }
+
+  // If the inbound frame is ARP, parse the payload as an ARPMessage
+  // an if successful, add the IP-to-Ethernet mapping to the mappings.
+  // In addition, if it's an ARP request asking for our IP address,
+  // send an appropriate ARP reply.
+  if ( frame.header.type == EthernetHeader::TYPE_ARP ) {
+    Parser parser( frame.payload );
+
+    ARPMessage arp_message;
+    arp_message.parse( parser );
+    if ( parser.has_error() )
+      return;
+    addMapping( time_elapsed_, arp_message.sender_ethernet_address, arp_message.sender_ip_address );
+    if ( arp_message.opcode == ARPMessage::OPCODE_REQUEST
+         && arp_message.target_ip_address == ip_address_.ipv4_numeric() ) {
+      EthernetFrame arp_reply = create_arp_message( ARPMessage::OPCODE_REPLY,
+                                                    ethernet_address_,
+                                                    ip_address_.ipv4_numeric(),
+                                                    arp_message.sender_ethernet_address,
+                                                    arp_message.sender_ip_address );
+      transmit( arp_reply );
+    }
+
+    // Since we have received an ARP message, we can probably send any queued datagrams
+    auto it = datagrams_queued_.begin();
+    while ( it != datagrams_queued_.end() ) {
+      if ( it->second.next_hop_ip == arp_message.sender_ip_address ) {
+        send_datagram( it->second.dgram, Address::from_ipv4_numeric( it->second.next_hop_ip ) );
+        it = datagrams_queued_.erase( it );
+      } else {
+        ++it;
+      }
+    }
+  }
 }
 
 //! \param[in] ms_since_last_tick the number of milliseconds since the last call to this method
 void NetworkInterface::tick( const size_t ms_since_last_tick )
 {
-  debug( "unimplemented tick({}) called", ms_since_last_tick );
+  // debug( "unimplemented tick({}) called", ms_since_last_tick );
+  time_elapsed_ += ms_since_last_tick;
+
+  remove_expired_mappings();
+
+  drop_expired_datagrams();
 }
--- src/network_interface.hh
+++ src/network_interface.hh
@@ -4,8 +4,10 @@
 #include "ethernet_frame.hh"
 #include "ipv4_datagram.hh"
 
+#include <map>
 #include <memory>
 #include <queue>
+#include <unordered_map>
 
 // A "network interface" that connects IP (the internet layer, or network layer)
 // with Ethernet (the network access layer, or link layer).
@@ -67,6 +69,10 @@ public:
   std::queue<InternetDatagram>& datagrams_received() { return datagrams_received_; }
 
 private:
+  static constexpr uint64_t MAPPING_TIMEOUT = 30000; // remember the IP-to-Ethernet mapping for 30 seconds
+
+  static constexpr uint64_t ARP_REQUEST_TIMEOUT = 5000; // Same ARP request can only resend after 5 seconds
+
   // Human-readable name of the interface
   std::string name_;
 
@@ -82,4 +88,115 @@ private:
 
   // Datagrams that have been received
   std::queue<InternetDatagram> datagrams_received_ {};
+
+  struct QueuedDatagram
+  {
+    uint32_t next_hop_ip {};
+    InternetDatagram dgram {};
+  };
+
+  // Datagrams that queued to learn the Ethernet address of the next hop
+  std::multimap<uint64_t, QueuedDatagram> datagrams_queued_ {};
+
+  uint64_t time_elapsed_ {}; // accumulated time elapsed
+
+  using MappingPair = std::pair<EthernetAddress, uint32_t>; // ethernet address and ip address
+
+  using NetworkMappings = std::multimap<uint64_t, MappingPair>; // timestamp, mappingpair
+
+  NetworkMappings mappings_ {};
+
+  std::unordered_map<uint32_t, uint64_t> last_arp_request_time_ {}; // ip address, timestamp
+
+  // function
+  void addMapping( uint64_t timestamp, const EthernetAddress& eth, uint32_t& ip )
+  {
+    mappings_.insert( make_pair( timestamp, MappingPair { eth, ip } ) );
+  }; // ip can be changed,but eth can't(const reference)
+
+  bool getMapping( uint32_t target_ip, EthernetAddress& eth )
+  {
+    for ( const auto& m : mappings_ ) {
+      if ( m.second.second == target_ip ) {
+        eth = m.second.first;
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // Remove expired mappings (older than 30 seconds)
+  void remove_expired_mappings()
+  {
+    if ( time_elapsed_ < MAPPING_TIMEOUT ) {
+      return;
+    }
+
+    // Calculate the expiration threshold
+    uint64_t expiration_threshold = time_elapsed_ - MAPPING_TIMEOUT;
+
+    // Find the first element that is not expired
+    // Since map is ordered by timestamp, we can remove all elements
+    // from beginning until we find a non-expired entry
+    auto it = mappings_.lower_bound( expiration_threshold );
+    mappings_.erase( mappings_.begin(), it );
+  }
+
+  // create an ARP request
+  EthernetFrame create_arp_message( uint16_t opcode,
+                                    const EthernetAddress& sender_eth,
+                                    uint32_t sender_ip,
+                                    const EthernetAddress& target_eth,
+                                    uint32_t target_ip )
+  {
+    EthernetFrame frame {};
+    if ( opcode == ARPMessage::OPCODE_REQUEST ) {
+      frame.header.dst = ETHERNET_BROADCAST;
+    } else {
+      frame.header.dst = target_eth;
+    }
+    frame.header.src = sender_eth;
+    frame.header.type = EthernetHeader::TYPE_ARP;
+
+    ARPMessage arp;
+    arp.opcode = opcode;
+    arp.sender_ethernet_address = sender_eth;
+    arp.sender_ip_address = sender_ip;
+    arp.target_ethernet_address = target_eth;
+    arp.target_ip_address = target_ip;
+
+    Serializer serializer;
+    arp.serialize( serializer );
+    frame.payload = serializer.finish();
+
+    return frame;
+  }
+
+  bool can_send_arp_request( uint32_t target_ip ) const
+  {
+    auto it = last_arp_request_time_.find( target_ip );
+    if ( it == last_arp_request_time_.end() ) {
+      return true;
+    }
+
+    return time_elapsed_ - it->second >= ARP_REQUEST_TIMEOUT;
+  }
+
+  void update_arp_request_time( uint32_t target_ip ) { last_arp_request_time_[target_ip] = time_elapsed_; }
+
+  // Drop any expired datagrams
+  void drop_expired_datagrams()
+  {
+    // Calculate the expiration threshold
+    uint64_t expiration_threshold = time_elapsed_ - ARP_REQUEST_TIMEOUT;
+    if ( time_elapsed_ < ARP_REQUEST_TIMEOUT ) {
+      return;
+    }
+
+    // Drop all expired datagrams
+    auto it = datagrams_queued_.begin();
+    while ( it != datagrams_queued_.end() && it->first <= expiration_threshold ) {
+      it = datagrams_queued_.erase( it );
+    }
+  }
 };
